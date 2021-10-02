2fa8c8472b57589c521101938bac9ab3f2292763
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-02 11:58:31 +0300

    Set up empty project

---
+++ src/test/java/com/pnu/cs/timeout/TimeoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.cs.timeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
