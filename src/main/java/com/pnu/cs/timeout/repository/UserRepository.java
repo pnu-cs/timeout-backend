3458c92de04bcb4f6aece64a3cf1955c7482bfe6
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-10 10:24:06 +0300

    Created User Repository

---
+++ src/main/java/com/pnu/cs/timeout/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pnu.cs.timeout.repository;
+
+import com.pnu.cs.timeout.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "select * from users where email =?1", nativeQuery = true)
+    User getUserByEmail(String email);
+}
