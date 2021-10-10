8310a52332b47dd23110e47eb6bf631e05446755
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-10 10:50:01 +0300

    Created UserService interface

---
+++ src/main/java/com/pnu/cs/timeout/service/UserService.java
@@ -0,0 +1,13 @@
+package com.pnu.cs.timeout.service;
+
+import com.pnu.cs.timeout.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User create(User user);
+    User readById(long id);
+    User update(User user);
+    void delete(long id);
+    List<User> getAll();
+}
