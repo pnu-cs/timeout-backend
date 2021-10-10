f4d1e807980906a4782d7572036d2ee898c73e45
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-10 10:23:51 +0300

    Created User model

---
+++ src/main/java/com/pnu/cs/timeout/model/User.java
@@ -0,0 +1,35 @@
+package com.pnu.cs.timeout.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Pattern(regexp = "^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$",
+            message = "Must be a valid e-mail address")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,32}$",
+            message = "Must contain at least one digit,lowercase and uppercase Latin character, special symbol and a length from 8 to 32")
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User() {
+    }
+}
