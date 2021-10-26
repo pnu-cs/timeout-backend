317513a14e225da3af4f6ac31c2b3a6cecd2858c
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-10-26 22:28:49 +0300

    Created UserDto for sign in

---
+++ src/main/java/com/pnu/cs/timeout/dto/UserDto.java
@@ -0,0 +1,36 @@
+package com.pnu.cs.timeout.dto;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Pattern;
+
+public class UserDto {
+
+    @Pattern(regexp = "^[\\\\w!#$%&'*+/=?`{|}~^-]+(?:\\\\.[\\\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,6}$",
+            message = "Must be a valid e-mail address")
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,32}$",
+            message = "Must contain at least one digit,lowercase and uppercase Latin character, special symbol and a length from 8 to 32")
+    private String password;
+
+    public UserDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
