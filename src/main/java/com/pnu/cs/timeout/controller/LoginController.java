218a42628f752472f90b65d4f1b97e84ebeecdd6
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-11-11 12:42:49 +0200

    Tried to change login requests to validate and produce proper results

--- src/main/java/com/pnu/cs/timeout/controller/LoginController.java
+++ src/main/java/com/pnu/cs/timeout/controller/LoginController.java
@@ -1,8 +1,15 @@
 package com.pnu.cs.timeout.controller;
 
 import com.pnu.cs.timeout.dto.UserDto;
+import com.pnu.cs.timeout.dto.UserTransformer;
+import com.pnu.cs.timeout.model.User;
 import com.pnu.cs.timeout.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,13 +22,23 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public UserDto login(@RequestBody UserDto userDto) {
-        return userService.readByEmail(userDto.getEmail());
+    public UserDto login(@Valid @RequestBody UserDto userDto, BindingResult result) {
+        if (result.hasErrors()) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        User userByEmail = userService.readByEmail(userDto.getEmail());
+
+        if (userByEmail != null && userByEmail.getPassword().equals(userDto.getPassword())) {
+            return UserTransformer.toDto(userByEmail);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
     @PostMapping("/register")
-    public UserDto register(@RequestBody UserDto userDto) {
-        return userService.create(userDto);
+    public UserDto register(@Valid @RequestBody UserDto userDto, BindingResult result) {
+        return UserTransformer.toDto(userService.create(UserTransformer.toEntity(userDto)));
     }
 
 }
