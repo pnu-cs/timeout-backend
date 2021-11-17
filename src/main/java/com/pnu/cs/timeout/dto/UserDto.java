e6a6b08477d63fdd34e34b5dc9061aefafe39b23
Mykhailo Pavliuk <pavliuk.mykhailo.dev@gmail.com>
2021-11-17 15:18:43 +0200

    Added necessary fields

--- src/main/java/com/pnu/cs/timeout/dto/UserDto.java
+++ src/main/java/com/pnu/cs/timeout/dto/UserDto.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.validation.constraints.Pattern;
 
 @Data
@@ -13,11 +15,20 @@ import javax.validation.constraints.Pattern;
 @Builder
 public class UserDto {
 
-    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\\\.[A-Za-z0-9-]+)*(\\\\.[A-Za-z]{2,})$",
+    @Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$",
             message = "Must be a valid e-mail address")
     private String email;
 
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,32}$",
             message = "Must contain at least one digit,lowercase and uppercase Latin character, special symbol and a length from 8 to 32")
     private String password;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    private String firstName;
+
+    @Pattern(regexp = "[A-Z][a-z]+",
+            message = "Must start with a capital letter followed by one or more lowercase letters")
+    private String lastName;
+
 }
